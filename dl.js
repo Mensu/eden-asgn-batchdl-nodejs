ca596c52b0c603cdb8a3cccdb87642e948304f6b
Mensu <yxshw55@qq.com>
2016-04-23 14:37:52 +0800

    download assignment by id; download standard answer
    attempt to add features: download assignment by id and download
    standard answer.

--- dl.js
+++ dl.js
@@ -27,49 +27,130 @@ function jQexec(body, cb) {
 	});
 }
 function FetchOne(Id, Title) {
-	console.log("Fetching", Id, Title);
+	if (Title.length == 0) console.log("Fetching Assignment", Id, "....");
+	else console.log("Fetching", Id, Title, "....");
 	request.get('http://eden.sysu.edu.cn/m/ass/' + Id, function(e, r, body) {
 		jQexec(body, function(err, window) {
 			var $ = window.$;
-			var folder = sanitize(Id + " " + Title),
-				devFile = "", isCpp = 0, i = 0;
+			var folder = "", devFile = "", isCpp = 0, i = 0;
+			  // set folder's name
+			if (Title.length == 0) folder = sanitize(Id);
+			else folder = sanitize(Id + " " + Title);
+
+			  // here I refer to the areas where to display and input codes as "block"
+
+			  /* there are four cases as far as blocks are concerned */
+			  	// 1. (before hard due) your answer
+			  	// 2. (before hard due) your answer + standard answer
+			  	// 3. (hard due passed) standard answer
+			  	// 4. no blocks
+			  // so I use blockTag (by checking whether it contains "hard due")
+			  // to distinguish whether the first block is "your answer"(no) or "standard answer"(yes)
+			  var blockTag = $('#tab-nondiv-container').prev().prev().text();
+			  var hardDue = false, myAnswer = false;
+			  if (~blockTag.indexOf('Hard due')) hardDue = true;
+			  if (~blockTag.indexOf('Your answer')) myAnswer = true;
+			  // the first block's blockTag is expected to be one of the two following sentences
+			    // Your answer:
+			        // => case 1 or case 2
+
+			    // Hard due has passed. The standard answer is unlocked:
+			        // => case 3
+			    
+			  // otherwise blockTag would be "" => case 4
+
+			  // case 4: there are no code files in this page. just return
+			if (!hardDue && !myAnswer) {
+				console.log("\nError: No code files exist. (the assignment id is " + Id + ")");
+				console.log("It is suggested that you check out whether the assignment actually exists or is in plagiarism pending.");
+				return;
+			}
+
+			  // deal with the first "block"
+			    // array of all filenames(in label <a>): $('#tab-nondiv-container .tab a')
 			$('#tab-nondiv-container .tab a').each(function() {
-				w = $(this).text();
+				var filename = "", code = "", subfolder = "";
+
+				  // raw filename
+				var w = $(this).text();
+
 				if (~w.indexOf("cpp")) isCpp = 1;
+
 				if (~w.indexOf('[*]')) {
-					filter = " textarea";
+					  // if the block contains the standard answer
+					if (hardDue) {
+						filter = " pre";
+						subfolder = "/Standard Answer/"
+					} else {  // if the block contains your answer
+						filter = " textarea";
+						code += "// To Be Finished\n";
+					}
 					filename = sanitize(w.replace('[*]', ''));
 				} else {
+					  // if the block contains the standard answer
+					if (hardDue) subfolder = "/Standard Answer/";
+
 					filter = " pre";
 					filename = sanitize(w);
 				}
-				code = $($(this).attr('href') + filter).text();
-				writeFile("./saved/" + folder + "/" + filename, code);
-				devFile += util.format("[Unit%d]\nFileName=%s\n", ++i, filename);
+				code += $($(this).attr('href') + filter).text();
+				writeFile("./saved/" + folder + "/" + subfolder + filename, code);
+				//devFile += util.format("[Unit%d]\nFileName=%s\n", ++i, filename);
 			});
-			devFile = util.format("[Project]\nFileName=Project%s.dev\nName=%s\n\
-				UnitCount=%s\nIsCpp=%d\nType=1\nVer=2\n",
-				Id, Id, $('#tab-nondiv-container .tab a').length, isCpp) + devFile;
-			writeFile("./saved/" + folder + "/Project" + Id + ".dev", devFile);
+
+			  // deal with the second block when there are both my answer block and std answer block
+			  // if the second block exists 
+			if ($('#tab-nondiv-containers').length == 1) {
+				$('#tab-nondiv-containers .tab a').each(function() {
+					var filename = "", code = "", subfolder = "/Standard Answer/", filter = " pre";
+					w = $(this).text();
+					if (~w.indexOf("cpp")) isCpp = 1;
+					if (~w.indexOf('[*]')) filename = sanitize(w.replace('[*]', ''));
+					else filename = sanitize(w);
+					code += $($(this).attr('href') + filter).text();
+					writeFile("./saved/" + folder + "/" + subfolder + filename, code);
+					//devFile += util.format("[Unit%d]\nFileName=%s\n", ++i, filename);
+			    });
+			}
+			console.log(".... Assignment", Id, "Downloaded Successfully");
+			//devFile = util.format("[Project]\nFileName=Project%s.dev\nName=%s\n\
+				//UnitCount=%s\nIsCpp=%d\nType=1\nVer=2\n",
+				//Id, Id, $('#tab-nondiv-container .tab a').length, isCpp) + devFile;
+			//writeFile("./saved/" + folder + "/Project" + Id + ".dev", devFile);
 		});
 	});
 }
-function doFetch() {
+function doFetch(id) {
 	request.get('http://eden.sysu.edu.cn/m/ass/', function(e, r, body) {
 		jQexec(body, function(err, window) {
-			var $ = window.$;
-			$('.item-ass a').each(function() {
-				Id = $(this).attr('href').replace(/[^0-9]/g, '');
-				Title = $(this).text().replace(/^\s+/, '').replace(/\s+$/, '');
-				FetchOne(Id, Title);
-			});
+			  // if id is valid
+			if (id.replace(/[^0-9]/g, '').length == 4 && id.length == 4) {
+				console.log("Ready to fetch the desired assignment (id = " + id + ")");
+				FetchOne(id, "");
+			} else {  // fetch unfinished assignments instead
+				console.log("Ready to fetch unfinished assignments");
+				var $ = window.$;
+				$('.item-ass a').each(function() {
+					// sanitize id and title
+					Id = $(this).attr('href').replace(/[^0-9]/g, '');
+					Title = $(this).text().replace(/^\s+/, '').replace(/\s+$/, '');
+					FetchOne(Id, Title);
+				});
+			}
 		})
 	});
 }
 
 function PromptLogin(csrf) {
 	prompt.start();
-	prompt.get(['username', {
+	  // welcome
+	console.log("\nPlease input the assignment id(opitional), username and password respectively.");
+	console.log("  *** Note: the assignment id should be a [four-digit] number.");
+	console.log("  *** If not, your unfinished assignments will be fetched instead\n"
+		+ "  *** which means that when required to input the assignment id you may\n"
+	    + "  *** [simply press Enter] to skip so as to fetch your unfinished assignments");
+	  // input id, username and password
+	prompt.get(['id', 'username', {
 		name: 'password',
 		hidden: true,
 		replace: '*',
@@ -95,8 +176,8 @@ function PromptLogin(csrf) {
 					console.log("login failed, please retry :(");
 					return PromptLogin(csrf);
 				} else {
-					console.log("logged in.... now fetching assignments");
-					doFetch();
+					console.log("logged in!");
+					doFetch(result.id);
 				}
 			});
 		})
